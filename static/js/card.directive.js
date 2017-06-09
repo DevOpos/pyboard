44dd15ec155ffddf824901f15463ce99728fbbf4
DevOpos <lafakiro@gmail.com>
2017-06-09 15:01:25 +0200

    Moving cards between lists added.

--- static/js/card.directive.js
+++ static/js/card.directive.js
@@ -10,12 +10,23 @@
             restrict: 'E',
             controller: ['$scope', '$http', function($scope, $http) {
                 var url = '/pyboard/cards/' + $scope.card.id + '/';
+
+                $scope.destList = $scope.list;
+
                 $scope.update = function () {
-                    $http.put(
+                    return $http.put(
                         url,
                         $scope.card
                     );
                 };
+
+                function removeCardFromList(card, list) {
+                    var cards = list.cards;
+                    cards.splice(
+                        cards.indexOf(card),
+                        1
+                    );
+                }
 
                 $scope.delete = function(){
                     $http.delete(url).then(
@@ -32,6 +43,20 @@
                 $scope.modelOptions = {
                     debounce: 500
                 };
+
+                $scope.move = function() {
+                    if ($scope.destList === undefined)
+                    {
+                        return;
+                    }
+                    $scope.card.list = $scope.destList.id;
+                    $scope.update().then(function(){
+                        {
+                            removeCardFromList($scope.card, $scope.list);
+                            $scope.destList.cards.push($scope.card);
+                        }
+                    });
+                }
             }]
         };
     }
